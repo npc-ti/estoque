a790ca13a4f2923fe64a4c94362a1bd089be6313
CaioFrSouza <caiodesouza12@gmail.com>
2021-01-31 19:07:59 -0300

    fix:allBugs

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,14 +1,15 @@
-import mongoose,{ Mongoose, connect, disconnect } from 'mongoose';
+import mongoose,{ Mongoose, disconnect } from 'mongoose';
+import { config } from 'dotenv';
 
 let db: typeof mongoose;
-
+config();
 class mongoDb {
     uri: string;
 
     constructor() {
         const { dbPass, dbName, dbUser } =  process.env;
-        this.uri = `mongodb+srv://${ dbUser }:${ dbPass }@cluster0.5ntvk.mongodb.net/${ dbName }/?Writes=true&w=majority`;
-        this.connection()
+        this.uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.5ntvk.mongodb.net/tests?retryWrites=true&w=majority`;
+        
     }
 
     public async connection (){
@@ -16,7 +17,7 @@ class mongoDb {
             if(db)
                 return db;
         
-        const connection = await connect(this.uri,{
+        const connection = await mongoose.connect(this.uri,{
             useNewUrlParser:true,
             useUnifiedTopology:true
           });

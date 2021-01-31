2e7e6d1a58aac37459937ed7cd65dfc14c71f99f
caioFrSouza <caiodesouza12@gmail.com>
2021-01-31 12:38:40 -0300

    add: connectionWithDataBase

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
 import express,{ Application, Request, Response } from 'express';
 import { config } from 'dotenv';
+import authRouters from './routes/authRouters';
+import Database from './db';
 
 config();
 const app: Application = express();
 const Port = process.env.PORT || 3000 ;
 
 app.use(express.json());
+app.use('/auth',authRouters);
+Database.connection();
 
-app.listen(Port, () => {
-    console.log(`O servidor esta rodando na porta ${Port}`)
-})
+app.listen(Port, async () => {
+    console.log(`O servidor esta rodando na porta ${Port}`);
+});

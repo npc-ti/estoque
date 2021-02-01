0169bf31e321accc1481b6cec8a3bdf28c491038
CaioFrSouza <caiodesouza12@gmail.com>
2021-02-01 12:35:24 -0300

    add: getCompanyMethod

--- server/src/controllers/company/index.ts
+++ server/src/controllers/company/index.ts
@@ -1,16 +1,32 @@
 import { Request, Response } from 'express';
+import companyStatic from '../../db/companys/company.static';
+import jwt from '../../middlewares/jwt';
 
-class Companys {
+class Company {
 
     private req: Request;
     private res: Response;
+    private companyIds: string[];
 
     constructor(req: Request, res: Response){
         this.req = req;
         this.res = res;
+        this.companyIds = jwt.companysId;
     }
 
-    public listAllCompanys(){
-        
+    public async getCompany(){
+        try{ 
+
+            const companys = this.companyIds.map(async (company)=>
+            await companyStatic.getCompany(company));
+
+            return this.res.status(200).send({companys});
+         }
+         catch(err){
+             return this.res.status(400).send({});
+         }
     }
+
 }
+
+export default (req: Request, res: Response) => new Company(req,res);

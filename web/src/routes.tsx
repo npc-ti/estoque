f3b522eac190252c5b90365b03aeb3bd75119967
CaioFrSouza <caiodesouza12@gmail.com>
2021-02-03 21:09:20 -0300

    create router auth and private router

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import SideBar from './components/sideBar';
+import UnfoundPage from './pages/404Page';
+import LoginPage from './pages/auth/login';
+import RegisterPage from './pages/auth/register';
 import HomePage from './pages/homePage';
+import { isAuthenticated } from './services/auth';
 
-const ApplicationRoutes: React.FC = () => {
-    return(
-        <BrowserRouter>
+const PrivateRoute = ({component: Component, rest}: any) => {
+    return <Route
+        {...rest}
+        render={(props: any) =>
+        isAuthenticated()? (
+            <Component {...props} />)
+        : (
+            <Redirect to="/login"/>)
+        }
+    >
+    </Route>
+}
+
+const DashRouters: React.FC = () => {
+    return (<>
         <SideBar/>
         <Switch>
-        <Route path="/" exact component={HomePage} />
+
+        <PrivateRoute path="/" exact component={HomePage} />
+
+        </Switch>
+    </>
+    )
+}
+
+
+
+
+const Routes: React.FC = () => {
+    return(
+        <BrowserRouter>
+        <Switch>
+        <PrivateRoute path="/" exact component={HomePage} />
+
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/register" exact component={RegisterPage} />
+        <Route component={UnfoundPage}/>
         </Switch>
         </BrowserRouter>
     )
 }
 
-export default ApplicationRoutes
+export default Routes
